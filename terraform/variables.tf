4966b7d48c320cb2531bc710f0677a693906dd05
LarryJr64 <wolfoung64@gmail.com>
2025-06-02 21:39:45 +0200

    Add initial Terraform and dbt configuration files

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "ID du projet GCP"
+  type        = string
+}
+
+variable "region" {
+  description = "Région GCP pour les ressources"
+  type        = string
+}
